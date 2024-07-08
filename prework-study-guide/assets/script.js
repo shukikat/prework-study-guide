8d31cb7deafd127e4375bdd870fd3025f15eb4cd
shukikat <kathuriashuki@gmail.com>
2024-07-08 10:30:51 -0400

    added javascript to project

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,24 @@
+const topics=["HTML", "CSS", "Javascript", "Git"];
+for(let x=0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+/*if (topic==='HTML') {
+    console.log("Let's study HTML");
+
+}
+else if (topic==='CSS'){
+    console.log ("Let's study CSS");
+}
+
+else if (topic==='Git') {
+    console.log("Let's study Git");
+}
+
+else if (topic==='Javascript'){
+    console.log("Let's Study Javascript");
+}
+
+else {
+    console.log("Please try again");
+} */
+
